698e43baed88bbbb29778787d04ff34b9a70dd56
Raytwo <Raytwost@gmail.com>
2020-05-28 22:56:31 +0200

    Forge refactor, hook_get_uncompressed_size, cleanup, minor version bump

--- src/forge.rs
+++ src/forge.rs
@@ -1,43 +1,95 @@
-use ktgl::feth::v120::linkdata::{ get_uncompressed_size };
 use ktgl::feth::v120::memory::kt_aligned_malloc;
 use lazy_static::lazy_static;
 use skyline::alloc::slice;
-use skyline::libc;
 use skyline::libc::c_void;
-use skyline::{hook, install_hook};
+use skyline::{hook, install_hooks};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
 use std::path::Path;
-use std::sync::Mutex;
 use std::{fs, io};
 
 lazy_static! {
-    static ref FORGE: Mutex<Forge> = Mutex::new(Forge::new());
+    pub static ref FORGE: Forge = Forge::new();
 }
 
-macro_rules! forge {
-    () => {
-        FORGE.lock().unwrap()
-    };
-}
-
-struct Forge {
+pub struct Forge {
     cache: HashMap<u32, String>,
 }
 
 impl Forge {
+    fn new() -> Forge {
+        let mut instance = Forge {
+            cache: HashMap::new(),
+        };
+
+        instance.visit_dirs(Path::new("sd:/Aldebaran/forge")).ok();
+
+        install_hooks!(hook_load_with_entry_id, hook_get_uncompressed_size);
+
+        instance
+    }
+
+    fn visit_file(&mut self, real_path: String, filename: String) {
+        let index_options = vec![
+            filename.parse::<u32>().ok(),
+            filename
+                .split("-")
+                .nth(0)
+                .and_then(|x| x.parse::<u32>().ok()),
+        ];
+        if let Some(index) = index_options.iter().filter_map(|&x| x).nth(0) {
+            println!("[Forge] Discovered {} => {}", index, real_path);
+            self.cache.insert(index, real_path);
+        }
+    }
+
+    fn visit_dirs(&mut self, dir: &Path) -> io::Result<()> {
+        if dir.is_dir() {
+            for entry in fs::read_dir(dir)? {
+                let entry = entry?;
+                let filename = entry.path(); // this looks wrong but is actually right
+                let real_path = format!("{}/{}", dir.to_str().unwrap(), filename.to_str().unwrap());
+                let path = Path::new(&real_path);
+                if path.is_dir() {
+                    self.visit_dirs(&path)?;
+                } else {
+                    self.visit_file(real_path, filename.to_str().unwrap().to_string());
+                }
+            }
+        }
+        Ok(())
+    }
+
     fn get_path_from_index(&self, index: u32) -> Option<&String> {
         self.cache.get(&index)
     }
 
-    fn new() -> Forge {
-        Forge {
-            cache: HashMap::new(),
+    pub fn get_filesize_for_fileid(&self, fileid: u32) -> Option<u64> {
+        let cached_path = match self.get_path_from_index(fileid) {
+            None => {
+                return None;
+            }
+            Some(path) => path,
+        };
+
+        let file = File::open(cached_path).unwrap();
+        let metadata = file.metadata().unwrap();
+
+        if metadata.is_dir() {
+            return None;
         }
+
+        Some(metadata.len())
     }
 
-    fn try_load(&self, entryid: u32, seek: u32, size: u64) -> Option<*const c_void> {
+    fn try_load(
+        &self,
+        entryid: u32,
+        seek: u32,
+        size: u64,
+        file_ptr: *const c_void,
+    ) -> Option<*const c_void> {
         let cached_path = self.get_path_from_index(entryid)?;
         println!("[Forge] EntryID {} loaded from {}.", entryid, cached_path);
         let mut file = File::open(cached_path).ok()?;
@@ -54,7 +106,12 @@ impl Forge {
         };
 
         unsafe {
-            let free_mem = kt_aligned_malloc(len, 0x10);
+            let free_mem: *const c_void;
+            if file_ptr != 0 as _ {
+                free_mem = file_ptr;
+            } else {
+                free_mem = kt_aligned_malloc(len, 0x10);
+            }
             let content = slice::from_raw_parts_mut(free_mem as *mut u8, len as usize);
             file.read_exact(content).ok()?;
             Some(content.as_ptr() as *const c_void)
@@ -73,7 +130,7 @@ pub fn hook_load_with_entry_id(
     unk2: u64,
     unk3: u64,
 ) -> *const c_void {
-    match forge!().try_load(entryid, seek, size) {
+    match FORGE.try_load(entryid, seek, size, file_ptr) {
         None => {
             println!("[Forge] EntryID {} loaded.", entryid);
             original!()(archive_ptr, entryid, file_ptr, seek, size, unk1, unk2, unk3)
@@ -82,65 +139,15 @@ pub fn hook_load_with_entry_id(
     }
 }
 
-#[hook(replace = get_uncompressed_size)]
+#[hook(offset = 0x4A0B40)]
 pub fn hook_get_uncompressed_size(linkdata_mgr: *const c_void, file_id: u32) -> u64 {
-    let forge_instance = forge!();
-    let cached_path = match forge_instance.get_path_from_index(file_id) {
-        None => {
-            return original!()(linkdata_mgr, file_id);
-        }
-        Some(filepath) => filepath,
+    let filesize = match FORGE.get_filesize_for_fileid(file_id) {
+        None => return original!()(linkdata_mgr, file_id),
+        Some(size) => size,
     };
-
-    let file = File::open(cached_path).ok().unwrap();
-    let metadata = file.metadata().ok().unwrap();
-
-    if metadata.is_dir() {
-        return original!()(linkdata_mgr, file_id);
-    }
-    let filesize = metadata.len();
-    println!("[Forge] Size of FileID {} requested, replying with {:x}", file_id, filesize);
+    println!(
+        "[Forge] Size of FileID {} requested, replying with {:#x}",
+        file_id, filesize
+    );
     filesize
-
-    
-}
-
-pub fn init_forge() {
-    install_hook!(hook_load_with_entry_id);
-    install_hook!(hook_get_uncompressed_size);
-
-    // we write a custom implementation of a directory tree walker because the implementation does not behave like spec
-    // which will make walkdir and friends fail
-
-    fn visit_file(real_path: String, filename: String) {
-        let index_options = vec![
-            filename.parse::<u32>().ok(),
-            filename
-                .split("-")
-                .nth(0)
-                .and_then(|x| x.parse::<u32>().ok()),
-        ];
-        if let Some(index) = index_options.iter().filter_map(|&x| x).nth(0) {
-            println!("[Forge] Discovered {} => {}", index, real_path);
-            forge!().cache.insert(index, real_path);
-        }
-    }
-
-    fn visit_dirs(dir: &Path) -> io::Result<()> {
-        if dir.is_dir() {
-            for entry in fs::read_dir(dir)? {
-                let entry = entry?;
-                let filename = entry.path(); // this looks wrong but is actually right
-                let real_path = format!("{}/{}", dir.to_str().unwrap(), filename.to_str().unwrap());
-                let path = Path::new(&real_path);
-                if path.is_dir() {
-                    visit_dirs(&path)?;
-                } else {
-                    visit_file(real_path, filename.to_str().unwrap().to_string());
-                }
-            }
-        }
-        Ok(())
-    }
-    visit_dirs(Path::new("sd:/Aldebaran/forge")).ok();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![feature(proc_macro_hygiene)]
-#![feature(asm)]
 
-use skyline::{hook, install_hook};
+use skyline::{c_str, hook, install_hook};
 mod forge;
+use lazy_static;
 
 // Required to prevent Skyline from crashing the game
 #[hook(offset = 0xAFCCF0)]
@@ -13,12 +13,15 @@ fn socket_stub() -> u32 {
 // Game::Menu::TitleScreen::GetVersionString()
 #[hook(offset = 0x3E63E0)]
 fn get_version_string() -> *const u64 {
-    String::from(format!("Aldebaran v{}\0", env!("CARGO_PKG_VERSION"))).as_ptr() as *const u64
+    c_str(&format!("Aldebaran v{}\0", env!("CARGO_PKG_VERSION"))) as _
 }
 
 #[skyline::main(name = "aldebaran")]
 pub unsafe fn main() {
-    println!("Hello from Aldebaran-rs!");
+    println!(
+        "Aldebaran-rs v{} - Fire Emblem Three Houses file replacement plugin",
+        env!("CARGO_PKG_VERSION")
+    );
 
     // Kill the original nn::os::SocketInitialize method
     install_hook!(socket_stub);
@@ -27,5 +30,5 @@ pub unsafe fn main() {
     install_hook!(get_version_string);
 
     // Intercept EntryId loadings to replace files on the fly
-    forge::init_forge();
+    lazy_static::initialize(&forge::FORGE);
 }
